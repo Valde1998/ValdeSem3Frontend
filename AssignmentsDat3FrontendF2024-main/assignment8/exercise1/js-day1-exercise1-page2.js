12ff2b642e508ada9171c221e427b67243873151
Valde1998 <124771370+Valde1998@users.noreply.github.com>
2024-04-26 22:30:52 +0200

    .

--- AssignmentsDat3FrontendF2024-main/assignment8/exercise1/js-day1-exercise1-page2.js
+++ AssignmentsDat3FrontendF2024-main/assignment8/exercise1/js-day1-exercise1-page2.js
@@ -124,10 +124,10 @@ document.getElementById("removeLast").addEventListener('click', function(event){
 
 let cars = [
     { id: 1, year: 1997, make: 'Ford', model: 'E350', price: 3000 },
-    { id: 2, year: 1999, make: 'Chevy', model: 'Venture', price: 4900 },
+    { id: 2, year: 1999, make: 'Chevy', model: 'Venture', price: 8000 },
     { id: 3, year: 2000, make: 'Chevy', model: 'Venture', price: 5000 },
     { id: 4, year: 1996, make: 'Jeep', model: 'Grand C', price: 4799 },
-    { id: 5, year: 2005, make: 'Volvo', model: 'V70', price: 44799 }
+    { id: 5, year: 2005, make: 'Volvo', model: 'V70', price: 9000 }
 ];
 
 function makingHtmlTable (cars){

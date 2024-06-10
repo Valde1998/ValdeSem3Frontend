7b63f9d214966b87d3f7f671259363754c02c5c0
Valde1998 <124771370+Valde1998@users.noreply.github.com>
2024-06-10 18:30:24 +0200

    .

--- EventBubbling/vite-project/src/App.jsx
+++ EventBubbling/vite-project/src/App.jsx
@@ -2,12 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Timer from './components/Timer'
 import ItemList from './mapsAndKeys'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <ItemList/>

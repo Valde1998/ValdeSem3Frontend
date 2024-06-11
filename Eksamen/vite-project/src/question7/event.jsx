02890692562cd97e9117b80bbe6c1420ad86e258
Valde1998 <124771370+Valde1998@users.noreply.github.com>
2024-06-11 19:18:37 +0200

    Create event.jsx

---
+++ Eksamen/vite-project/src/question7/event.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const EventHandlingExample = () => {
+  const handleClick = (event) => {
+    console.log('Button clicked!', event);
+    console.log('Event type:', event.type); // SyntheticEvent type
+  };
+
+  return (
+    <button onClick={handleClick}>Klik mig!</button>
+  );
+};
+
+export default EventHandlingExample;
